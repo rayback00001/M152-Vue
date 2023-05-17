e84f532fa9b412216a99e8ee2cbbc75db9ebc282
Rakul <rabrosrajgopu@gmail.com>
2023-05-17 14:33:10 +0200

    added task 2 + 3

--- src/views/Pages/Kasse.vue
+++ src/views/Pages/Kasse.vue
@@ -24,6 +24,32 @@ const currentId = ref(2)
 function roundTo05(number){
     return (Math.ceil(number*20)/20).toFixed(2);
 }
+
+const newText = ref();
+const newValue = ref();
+
+function addEntry(){
+    const newElement = {
+        text: newText.value,
+        value: parseFloat(newValue.value),
+        id: ++currentId.value
+    }
+
+    log.value.push(newElement);
+
+    newText.value = '';
+    newValue.value = '';
+}
+
+function deleteLog(id) { 
+    //log.value = log.value.filter(entry => entry.id !== id);
+
+    log.value = log.value.filter((element)=>{
+        return element.id !== id
+    })
+
+ }
+
 </script>
 
 <template>
@@ -37,12 +63,35 @@ function roundTo05(number){
     </thead>
     <tbody>
         <tr v-for="logEntry in log" :key="logEntry.id" :class="logEntry.value > 0 ? 'text-green' : 'text-red'">
-            <td class="text-left">{{logEntry.text}}</td>
-            <td class="text-right"><span v-if="logEntry.value > 0">{{roundTo05(logEntry.value)}} CHF</span></td>
-            <td class="text-right"><span v-if="logEntry.value < 0">{{roundTo05(logEntry.value)}} CHF</span></td>
+            <td>{{logEntry.text}}</td>
+            <td><span v-if="logEntry.value > 0">{{roundTo05(logEntry.value)}} CHF</span></td>
+            <td><span v-if="logEntry.value < 0">{{roundTo05(logEntry.value)}} CHF</span></td>
+            <td><button @click="deleteLog(logEntry.id)">Eintrag löschen</button></td>
         </tr>
     </tbody>
 </table>
+
+<div class="wrapper">
+    <h2>Neuer Eintrag erfassen</h2>
+    <div class="form">
+        <div class="form-row">
+            <label for="text">Text</label>
+            <input type="text" id="text" name="text" v-model="newText" @keyup.enter="addEntry">
+        </div>
+
+        <div class="form-row">
+            <label for="amount">Betrag</label>
+            <input type="number" id="amount" name="amount" v-model="newValue" @keyup.enter="addEntry">
+        </div>
+
+        <div class="form-row">
+            <button @click="addEntry">Speichern</button>
+        </div>
+
+
+    </div>
+</div>
+
 
 </template>
 
@@ -51,22 +100,74 @@ function roundTo05(number){
 table{
     width: 500px;
     margin: 0 auto;
-}
-
-tr th{
-     border-bottom: 2px solid darkgray;
-}
-
-tr td{
-    border-bottom: 1px solid grey;
+    margin-top: 50px;
 }
 
 
-.text-right{
-    text-align: right;
+.wrapper{
+    width: 500px;
+    margin: 0 auto; 
 }
 
-.text-left{
+.form{
+    .form-row{
+        display: flex;
+        margin-bottom: 10px;
+
+        label{
+            width:30%;
+        }
+
+    }
+}
+
+
+table,
+th,
+td {
+    border-collapse: collapse;
+    padding: 1rem;
     text-align: left;
 }
+
+thead th {
+    position: sticky;
+    top: 0;
+    left: 0;
+    background-color: #d5d1defe;
+    cursor: pointer;
+    text-transform: capitalize;
+}
+
+tbody tr:nth-child(even) {
+    background-color: #0000000b;
+}
+
+tbody tr {
+    --delay: .1s;
+    transition: .5s ease-in-out var(--delay), background-color 0s;
+}
+
+tbody tr.hide {
+    opacity: 0;
+    transform: translateX(100%);
+}
+
+tbody tr:hover {
+    background-color: #fff6 !important;
+}
+
+tbody tr td,
+tbody tr td p,
+tbody tr td img {
+    transition: .2s ease-in-out;
+}
+
+tbody tr.hide td,
+tbody tr.hide td p {
+    padding: 0;
+    font: 0 / 0 sans-serif;
+    transition: .2s ease-in-out .5s;
+}
+
 </style>
